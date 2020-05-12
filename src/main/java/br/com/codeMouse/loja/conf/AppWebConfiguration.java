2e32a01487018dac26bd5546b65abc1676181811
hshimada <shimada.henrique@gmail.com>
2020-05-11 22:23:31 -0300

    Package naming adjust

--- src/main/java/br/com/codeMouse/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/codeMouse/loja/conf/AppWebConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import br.com.codeMouse.loja.controllers.HomeController;
+import br.com.codeMouse.loja.controller.HomeController;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses = {HomeController.class})
--- src/main/java/br/com/codeMouse/loja/controller/HomeController.java
+++ src/main/java/br/com/codeMouse/loja/controller/HomeController.java
@@ -1,4 +1,4 @@
-package br.com.codeMouse.loja.controllers;
+package br.com.codeMouse.loja.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
