d4998cee9a70d3564c7e800bf374196e433050f5
hshimada <shimada.henrique@gmail.com>
2020-05-16 23:57:16 -0300

    Added a FlashAttribute to a HTTPRequest call. Reload won't be redundant

--- src/main/java/br/com/codeMouse/store/controller/ProductController.java
+++ src/main/java/br/com/codeMouse/store/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.codeMouse.store.dao.ProductDAO;
 import br.com.codeMouse.store.model.PriceType;
@@ -31,19 +32,24 @@ public class ProductController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String save(Product product) {
+	public ModelAndView save(Product product, RedirectAttributes redirectAttributes) {
 		System.out.println(product);
-		/*
-		 * persists Product object through .save() from ProductDAO's injected Class
-		 */
+//		persists Product object through .save() from ProductDAO's injected Class
 		productDAO.save(product);
-		return "/products/ok";
+		redirectAttributes.addFlashAttribute("success", "Product successfully registered"); // passes a request valid
+																							// for a single request to
+																							// the next HTTP call that
+																							// returns a success message
+																							// to the JSP
+
+//		return "/products/ok"; // stops returning for the same page and passes by redirect, so method called are always GET and doesn't keeps calling the same post call and adding the same data to the database
+		return new ModelAndView("redirect:products");
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView list() {
 		List<Product> products = productDAO.list();
-		ModelAndView modelAndView = new ModelAndView("products/list"); //parses a Model object to the view
+		ModelAndView modelAndView = new ModelAndView("products/list"); // parses a Model object to the view
 		modelAndView.addObject("products", products);
 		return modelAndView;
 	}
